85efcb72179ef382fc387e6187805d680aa76dfe
Mukta G. Palshikar <32854513+muktapalshikar@users.noreply.github.com>
2025-12-15 12:02:33 -0500

    MERFISH cell typing

---
+++ Cell typing/Step 2 - Fine_typing/Fine_typing_with_weighted_KNN/B/fine_typing_B.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -c 1
+#SBATCH -t 0-12:00
+#SBATCH -p short
+#SBATCH -o B.out
+#SBATCH -e B.err
+#SBATCH --mem=200G
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=B_fine_typing
+
+module load gcc/9.2.0 R/4.1.2 python/3.10.11 geos/3.10.2 cmake/3.22.2 gdal/3.1.4 udunits/2.2.28 
+source /home/mup728/jupytervenv/bin/activate
+
+Rscript fine_typing_all_B_cells.r
+
---
+++ Cell typing/Step 2 - Fine_typing/Fine_typing_with_weighted_KNN/Strom/fine_typing_strom.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -c 1
+#SBATCH -t 0-12:00
+#SBATCH -p short
+#SBATCH -o strom.out
+#SBATCH -e strom.err
+#SBATCH --mem=200G
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=strom_fine_typing
+
+module load gcc/9.2.0 R/4.1.2 python/3.10.11 geos/3.10.2 cmake/3.22.2 gdal/3.1.4 udunits/2.2.28 
+source /home/mup728/jupytervenv/bin/activate
+
+Rscript fine_typing_all_stromal_cells.r
+
+
